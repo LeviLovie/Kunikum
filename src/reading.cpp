281c5cdbf9860e9346eea1a556042f8625f80d0f
leviiloviee <107021730+LeviiLovie@users.noreply.github.com>
2023-01-08 22:55:52 +0700

    Create .kun UPDATE function & Move line compiler to function

--- src/reading.cpp
+++ src/reading.cpp
@@ -1,8 +1,10 @@
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <map>
+#include <unistd.h>
 
 #include "reading.h"
 #include "VRAM.h"
@@ -12,9 +14,10 @@ std::string WarnColor = "\033[0;33m";
 std::string ResetColor = "\033[0;37m";
 
 int tabSize = 2;
-int FPS = 1;
+int FPS;
 
 std::string updateCommands[4096];
+std::string initCommands[4096];
 
 std::map<std::string, int>* intVars = new std::map<std::string, int>;
 std::map<std::string, std::string>* strVars = new std::map<std::string, std::string>;
@@ -27,80 +30,34 @@ void CompileFile(std::string FileName) {
     std::string method = "";
     int step = 0;
     int lineNumber = 0;
-
-    int updateLineNumber = 0;
+    int startLinneNumber = 0;
 
     while (getline(infile, line)) {
-        lineNumber++;
-        if (line.find("TABSIZE") == 0) {
-            newline = line.substr(8);
-            if (newline.find("=") == 0) {
-                newline = newline.substr(2);
-                if (newline.find(";") == newline.length() - 1) {
-                    newline = newline.substr(0, newline.length() - 1);
-                    tabSize = std::stoi(newline);
-                    std::cout << tabSize << std::endl;
-                } else {
-                    std::cout << WarnColor << lineNumber << " - Missing semicolon: \"" << line << "\"" << ResetColor << std::endl;
-                }
-            } else {
-                std::cout << WarnColor << lineNumber << " - Missing equal sign: \"" << line << "\"" << ResetColor << std::endl;
-            }
-        }
-        if (line.find("FPS") == 0) {
-            newline = line.substr(4);
-            if (newline.find("=") == 0) {
-                newline = newline.substr(2);
-                if (newline.find(";") == newline.length() - 1) {
-                    newline = newline.substr(0, newline.length() - 1);
-                    FPS = std::stoi(newline);
-                    std::cout << FPS << std::endl;
-                } else {
-                    std::cout << WarnColor << lineNumber << " - Missing semicolon: \"" << line << "\"" << ResetColor << std::endl;
-                }
-            } else {
-                std::cout << WarnColor << lineNumber << " - Missing equal sign: \"" << line << "\"" << ResetColor << std::endl;
-            }
-        }
-
         if (line.find("#") != std::string::npos || line == "") {
             continue;
         } else {
             if (method == "") {
                 if (line.find("}") == 0) {
-                    if (step > 0) {
+                    if (step > 1) {
+                        step--;
+                    } else if (step == 1) {
+                        startLinneNumber = 0;
+                        method = "";
                         step--;
                     } else {
-                        std::cout << ErrorColor << lineNumber << " - Doesn't statement to close" << ResetColor << std::endl;
+                        std::cout << ErrorColor << lineNumber << ":1 - Doesn't statement to close" << ResetColor << std::endl;
+                        std::exit(0);
                     }
                 } else if (line.find("ENGINE") == 0) {
                     newline = line.substr(7);
-                    std::cout << newline << std::endl;
-                    if (newline.find("INIT" == 0)) {
-                        newline = newline.substr(4);
-                        if (newline.find("() {") == 0) {
-                            if (newline != "") {
-                                method = "INIT";
-                                step++;
-                            } else {
-                                std::cout << WarnColor << lineNumber << " - can't compille code after \"() {\": \"" << line << "\"" << ResetColor << std::endl;
-                            }
-                        } else {
-                            std::cout << WarnColor << lineNumber << " - function start missing: \"" << line << "\"" << ResetColor << std::endl;
-                        }
-                    } else if (newline.find("UPDATE" == 0)) {
-                        newline = newline.substr(6);
-                        std::cout << newline << std::endl;
-                        if (newline.find("() {") == 0) {
-                            if (newline != "") {
-                                method = "UPDATE";
-                                step++;
-                            } else {
-                                std::cout << WarnColor << lineNumber << " - can't compille code after \"() {\": \"" << line << "\"" << ResetColor << std::endl;
-                            }
-                        } else {
-                            std::cout << WarnColor << lineNumber << " - function start missing: \"" << line << "\"" << ResetColor << std::endl;
-                        }
+                    if (newline == "INIT() {") {
+                        method = "INIT";
+                        startLinneNumber = lineNumber;
+                        step++;
+                    } else if (newline == "UPDATE() {") {
+                        method = "UPDATE";
+                        startLinneNumber = lineNumber;
+                        step++;
                     } else {
                         std::cout << WarnColor << lineNumber << " - Unkow method name: \"" << line << "\"" << ResetColor << std::endl;
                     }
@@ -108,102 +65,158 @@ void CompileFile(std::string FileName) {
                     std::cout << WarnColor << lineNumber << " - Unknow line syntax: \"" << line << "\"" << ResetColor << std::endl;
                 }
             } else if (method == "INIT") {
-                if (line.find("LOG") == tabSize) {
-                    newline = line.substr(tabSize + 3);
-                    if (newline.find("(") == 0) {
-                        newline = newline.substr(1);
-                        if (newline.find(");") == newline.length() - 2) {
-                            newline = newline.substr(1, newline.length() - 4);
-                            std::cout << newline << std::endl;
-                        } else {
-                            std::cout << WarnColor << lineNumber << " - function end missing: \"" << line << "\"" << ResetColor << std::endl;
-                        }
-                    } else {
-                        std::cout << WarnColor << lineNumber << " - function start missing: \"" << line << "\"" << ResetColor << std::endl;
-                    }
-                } else if (line.find("VRAM.SET") == tabSize) {
-                    newline = line.substr(tabSize + 8);
+                if (line.find("SETFPS") == 2) {
+                    newline = line.substr(8);
                     if (newline.find("(") == 0) {
                         newline = newline.substr(1);
                         if (newline.find(");") == newline.length() - 2) {
                             newline = newline.substr(0, newline.length() - 2);
-                            short args[3];
-                            short arg = 0;
-                            std::stringstream ss(newline);
-                            std::string item;
-                            while (getline(ss, item, ',')) {
-                                std::stringstream token_stream(item);
-                                token_stream >> item;
-                                args[arg] = std::stoi(item);
-                                arg++;
-                            }
-                            SetPixel(args[0], args[1], args[2]);
+                            FPS = std::stoi(newline);
                         } else {
                             std::cout << WarnColor << lineNumber << " - function end missing: \"" << line << "\"" << ResetColor << std::endl;
                         }
                     } else {
                         std::cout << WarnColor << lineNumber << " - function start missing: \"" << line << "\"" << ResetColor << std::endl;
                     }
-                } else if (line.find("VRAM.RECTSET") == tabSize) {
-                    newline = line.substr(tabSize + 12);
+                } else if (line.find("SETUPDATETIMES") == 2) {
+                    newline = line.substr(8);
                     if (newline.find("(") == 0) {
                         newline = newline.substr(1);
                         if (newline.find(");") == newline.length() - 2) {
                             newline = newline.substr(0, newline.length() - 2);
-                            short args[5];
-                            short arg = 0;
-                            std::stringstream ss(newline);
-                            std::string item;
-                            while (getline(ss, item, ',')) {
-                                std::stringstream token_stream(item);
-                                token_stream >> item;
-                                args[arg] = std::stoi(item);
-                                arg++;
-                            }
-                            SetPixels(args[0], args[1], args[2], args[3], args[4]);
+                            FPS = std::stoi(newline);
                         } else {
                             std::cout << WarnColor << lineNumber << " - function end missing: \"" << line << "\"" << ResetColor << std::endl;
                         }
                     } else {
                         std::cout << WarnColor << lineNumber << " - function start missing: \"" << line << "\"" << ResetColor << std::endl;
                     }
-                } else if (line.find("}") == 0) {
-                    if (step > 0) {
-                        method = "";
-                        step--;
-                    } else {
-                        std::cout << ErrorColor << lineNumber << " - Doesn't statement to close" << ResetColor << std::endl;
-                    }
+                } else if (line != "}") {
+                    initCommands[startLinneNumber] = line.substr(2, line.length());
+                    startLinneNumber++;
                 } else {
-                    std::cout << WarnColor << lineNumber << " - Unknow line syntax: \"" << line << "\"" << ResetColor << std::endl;
-                } 
-            } else if (method == "UPDATE") {
-                std::cout << "Update" << std::endl;
-                if (line.find("}") == 0) {
-                    if (step > 0) {
-                        method = "";
-                        step--;
-                    } else {
-                        std::cout << ErrorColor << lineNumber << " - Doesn't statement to close" << ResetColor << std::endl;
-                    }
-                } else {
-                    updateCommands[updateLineNumber] = line.substr(0, line.length());
+                    step--;
+                    startLinneNumber = 0;
+                    method = "";
                 }
-                updateLineNumber++;
+            } else if (method == "UPDATE") {
+                if (line != "}") {
+                    updateCommands[startLinneNumber] = line.substr(2, line.length());
+                    startLinneNumber++;
+                } else {
+                    step--;
+                    startLinneNumber = 0;
+                    method = "";
+                }
             }
         }
+        lineNumber++;
     }
     if (step > 0) {
-        std::cout << ErrorColor << lineNumber << " - Doesn't statement to close" << ResetColor << std::endl;
+        std::cout << ErrorColor << lineNumber << ":3 - Doesn't statement to close" << ResetColor << std::endl;
+        std::exit(0);
     }
 
-    Update();
+    Init();
+
+    while (true)
+    {
+        Update();
+        // sleep((1000 / FPS) * 1000);
+        // sleep(1 / FPS);
+        usleep(1000000 / FPS);
+    }
+}
+
+void Init() {
+    int step = 0;
+    for (int i = 0; i < 4096; i++) {
+        if (initCommands[i] != "") {
+            step = WorkOnLine(initCommands[i], "INIT", i, step);
+        }
+    }
 }
 
 void Update() {
-    for (int i = 0; i < 4096; i++) {
+    int step = 0;
+    for (int i = 0; i< 4096; i++) {
         if (updateCommands[i] != "") {
-            std::cout << updateCommands[i] << std::endl;
+            step = WorkOnLine(updateCommands[i], "UPDATE", i, step);
         }
     }
+    SaveVRAM();
+}
+
+int WorkOnLine(std::string line, std::string method, int lineNumber, int step) {
+    std::string newline;
+    if (line.find("LOG") == 0) {
+        newline = line.substr(3);
+        if (newline.find("(") == 0) {
+            newline = newline.substr(1);
+            if (newline.find(");") == newline.length() - 2) {
+                newline = newline.substr(1, newline.length() - 4);
+                std::cout << lineNumber << ":" << method << " - \"" << newline << "\"" << std::endl;
+            } else {
+                std::cout << WarnColor << lineNumber << ":(INIT) - function end missing: \"" << line << "\"" << ResetColor << std::endl;
+            }
+        } else {
+            std::cout << WarnColor << lineNumber << ":(INIT) - function start missing: \"" << line << "\"" << ResetColor << std::endl;
+        }
+    } else if (line.find("VRAM.SET") == 0) {
+        newline = line.substr(8);
+        if (newline.find("(") == 0) {
+            newline = newline.substr(1);
+            if (newline.find(");") == newline.length() - 2) {
+                newline = newline.substr(0, newline.length() - 2);
+                short args[3];
+                short arg = 0;
+                std::stringstream ss(newline);
+                std::string item;
+                while (getline(ss, item, ',')) {
+                    std::stringstream token_stream(item);
+                    token_stream >> item;
+                    args[arg] = std::stoi(item);
+                    arg++;
+                }
+                SetPixel(args[0], args[1], args[2]);
+            } else {
+                std::cout << WarnColor << lineNumber << ":(INIT) - function end missing: \"" << line << "\"" << ResetColor << std::endl;
+            }
+        } else {
+            std::cout << WarnColor << lineNumber << ":(INIT) - function start missing: \"" << line << "\"" << ResetColor << std::endl;
+        }
+    } else if (line.find("VRAM.RECTSET") == 0) {
+        newline = line.substr(12);
+        if (newline.find("(") == 0) {
+            newline = newline.substr(1);
+            if (newline.find(");") == newline.length() - 2) {
+                newline = newline.substr(0, newline.length() - 2);
+                short args[5];
+                short arg = 0;
+                std::stringstream ss(newline);
+                std::string item;
+                while (getline(ss, item, ',')) {
+                    std::stringstream token_stream(item);
+                    token_stream >> item;
+                    args[arg] = std::stoi(item);
+                    arg++;
+                }
+                SetPixels(args[0], args[1], args[2], args[3], args[4]);
+            } else {
+                std::cout << WarnColor << lineNumber << ":(INIT) - function end missing: \"" << line << "\"" << ResetColor << std::endl;
+            }
+        } else {
+            std::cout << WarnColor << lineNumber << ":(INIT) - function start missing: \"" << line << "\"" << ResetColor << std::endl;
+        }
+    } else if (line.find("}") == 0) {
+        if (step > 0) {
+            step--;
+        } else {
+            std::cout << ErrorColor << lineNumber << ":(INIT) - Doesn't statement to close" << ResetColor << std::endl;
+            std::exit(0);
+        }
+    } else {
+        std::cout << WarnColor << lineNumber << ":(INIT) - Unknow line syntax: \"" << line << "\"" << ResetColor << std::endl;
+    }
+    return step;
 }
--- src/reading.h
+++ src/reading.h
@@ -4,3 +4,5 @@
 
 void CompileFile(std::string FileName);
 void Update();
+void Init();
+int WorkOnLine(std::string line, std::string method, int lineNumber, int step);
